0a720bf25e3d6b6e42b76035494524e67bb0383c
xiaotang158 <66454911+xiaotang158@users.noreply.github.com>
2025-02-07 06:33:27 +0800

    Update VIPBanner.java

--- app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
@@ -1,27 +1,12 @@
 package com.shatyuka.zhiliao.hooks;
 
-import android.content.Context;
-import android.content.res.XmlResourceParser;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
-
-import com.shatyuka.zhiliao.Helper;
-import com.shatyuka.zhiliao.R;
-
-import java.lang.reflect.Method;
-
-import de.robv.android.xposed.XC_MethodReplacement;
+import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
-import de.robv.android.xposed.XposedHelpers;
 
 public class VIPBanner implements IHook {
-    static Class<?> VipEntranceView;
-    static Class<?> MoreVipData;
-    static Class<?> NewMoreFragment;
-
-    static Method initView;
-    static Method initView_new;
+     static Class<?> MoreHybridView;
+     static Class<?> ZHRecyclerView;
 
     @Override
     public String getName() {
@@ -30,58 +15,33 @@ public class VIPBanner implements IHook {
 
     @Override
     public void init(ClassLoader classLoader) throws Throwable {
-        try {
-            VipEntranceView = classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.more.widget.VipEntranceView");
-            initView = VipEntranceView.getDeclaredMethod("a", Context.class);
-        } catch (ClassNotFoundException ignored) {
-            VipEntranceView = classLoader.loadClass("com.zhihu.android.premium.view.VipEntranceView");
-            initView_new = VipEntranceView.getDeclaredMethod("initView", Context.class);
-        }
-
-        try {
-            MoreVipData = classLoader.loadClass("com.zhihu.android.api.MoreVipData");
-            NewMoreFragment = classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.more.NewMoreFragment");
-        } catch (ClassNotFoundException ignored) {
-        }
+        MoreHybridView = classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.more.widget.MoreHybridView");
+        ZHRecyclerView = classLoader.loadClass("com.zhihu.android.base.widget.ZHRecyclerView");
     }
 
     @Override
     public void hook() throws Throwable {
-        if (Helper.prefs.getBoolean("switch_mainswitch", false) && Helper.prefs.getBoolean("switch_vipbanner", false)) {
-            if (initView != null) {
-                XposedBridge.hookMethod(initView, new XC_MethodReplacement() {
+        if (Helper.prefs != null &&
+            Helper.prefs.getBoolean("switch_mainswitch", false) &&
+            Helper.prefs.getBoolean("switch_vipbanner", false)) {
+
+            
+                XposedBridge.hookAllMethods(MoreHybridView, "onAttachedToWindow", new XC_MethodHook() {
                     @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) {
-                        XmlResourceParser layout_vipentranceview = Helper.modRes.getLayout(R.layout.layout_vipentranceview);
-                        LayoutInflater.from((Context) param.args[0]).inflate(layout_vipentranceview, (ViewGroup) param.thisObject);
-                        return null;
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        ((View) param.thisObject).setVisibility(View.GONE);
                     }
                 });
-            }
-            if (initView_new != null) {
-                XposedBridge.hookMethod(initView_new, new XC_MethodReplacement() {
+            
+
+            
+                XposedBridge.hookAllMethods(ZHRecyclerView, "onAttachedToWindow", new XC_MethodHook() {
                     @Override
-                    protected Object replaceHookedMethod(MethodHookParam param) {
-                        XmlResourceParser layout_vipentranceview = Helper.modRes.getLayout(R.layout.layout_vipentranceview_new);
-                        LayoutInflater.from((Context) param.args[0]).inflate(layout_vipentranceview, (ViewGroup) param.thisObject);
-                        return null;
+                    protected void afterHookedMethod(MethodHookParam param) {
+                        ((View) param.thisObject).setVisibility(View.GONE);
                     }
                 });
-            }
-            for (Method method : VipEntranceView.getMethods()) {
-                if (method.getName().equals("setData")) {
-                    XposedBridge.hookMethod(method, XC_MethodReplacement.returnConstant(null));
-                    break;
-                }
-            }
-            XposedHelpers.findAndHookMethod(VipEntranceView, "onClick", View.class, XC_MethodReplacement.returnConstant(null));
-            XposedBridge.hookAllMethods(VipEntranceView, "resetStyle", XC_MethodReplacement.returnConstant(null));
-
-            if (MoreVipData != null && NewMoreFragment != null) {
-                XposedHelpers.findAndHookMethod(NewMoreFragment, "a", MoreVipData, XC_MethodReplacement.returnConstant(null));
-            }
-
-            XposedBridge.hookAllMethods(MoreVipData, "isLegal", XC_MethodReplacement.returnConstant(Boolean.FALSE));
+            
         }
     }
 }

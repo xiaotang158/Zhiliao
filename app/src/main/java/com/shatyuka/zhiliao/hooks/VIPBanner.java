96f0b331128d7db116890147e62a3b5404d5ad51
xiaotang158 <66454911+xiaotang158@users.noreply.github.com>
2025-02-07 21:51:50 +0800

    Update VIPBanner.java

--- app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
@@ -9,16 +9,15 @@ import com.shatyuka.zhiliao.Helper;
 
 public class VIPBanner implements IHook {
      static Class<?> MoreHybridView;
-     static Class<?> ZHRecyclerView;
+    // static Class<?> ZHRecyclerView;
      static Method MonAttachedToWindow;
-     static Method ZonAttachedToWindow;
+    // static Method ZonAttachedToWindow;
 
      boolean MoreHybridStat = true;
      boolean ZHRecyclerStat = true;
 
     @Override
     public String getName() {
-                                                           XposedBridge.log("[隐藏会员卡片] " );
 
         return "隐藏会员卡片";
     }
@@ -26,7 +25,7 @@ public class VIPBanner implements IHook {
     @Override
     public void init(ClassLoader classLoader) throws Throwable {
         MoreHybridView = classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.more.widget.MoreHybridView");
-        ZHRecyclerView = classLoader.loadClass("com.zhihu.android.base.widget.ZHRecyclerView");
+       // ZHRecyclerView = classLoader.loadClass("com.zhihu.android.base.widget.ZHRecyclerView");
   
 
       
@@ -40,10 +39,8 @@ public class VIPBanner implements IHook {
        XposedBridge.hookAllMethods(MoreHybridView, "onFinishInflate", new XC_MethodHook() {
                 @Override
                 protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                    XposedBridge.log("[Zhiliao1] " + param);
                     super.afterHookedMethod(param);
                     if (MoreHybridStat) {
-                        XposedBridge.log("[Zhiliao2] " + param.thisObject);
                         View view = (View) param.thisObject;
                         view.setVisibility(View.GONE);
                         MoreHybridStat = false;
@@ -51,31 +48,31 @@ public class VIPBanner implements IHook {
                 }
             });
 
-       XposedBridge.hookAllMethods(ZHRecyclerView, "onViewAdded", new XC_MethodHook() {
+       // XposedBridge.hookAllMethods(ZHRecyclerView, "onViewAdded", new XC_MethodHook() {
 
-                @Override
-                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                                                      XposedBridge.log("[Zhiliao3] " + param);
+       //          @Override
+       //          protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+       //                                                XposedBridge.log("[Zhiliao3] " + param);
 
-                    super.afterHookedMethod(param);
-                    if (ZHRecyclerStat && param.thisObject.getClass().getName().equals(ZHRecyclerView.getName())) {
-                                                                                   XposedBridge.log("[Zhiliao4] " + param.thisObject);
+       //              super.afterHookedMethod(param);
+       //              if (ZHRecyclerStat && param.thisObject.getClass().getName().equals(ZHRecyclerView.getName())) {
+       //                                                                             XposedBridge.log("[Zhiliao4] " + param.thisObject);
 
-                        View view = (View) param.thisObject;
-                        try {
-                            String name = view.getContext().getResources().getResourceEntryName(view.getId());
-                            if ("function_panel".equals(name) ||
-                                "new_function_panel".equals(name) ||
-                                "common_items".equals(name) ||
-                                "new_common_items".equals(name)) {
+       //                  View view = (View) param.thisObject;
+       //                  try {
+       //                      String name = view.getContext().getResources().getResourceEntryName(view.getId());
+       //                      if ("function_panel".equals(name) ||
+       //                          "new_function_panel".equals(name) ||
+       //                          "common_items".equals(name) ||
+       //                          "new_common_items".equals(name)) {
                                 
-                                view.setVisibility(View.GONE);
-                                ZHRecyclerStat = false;
-                            }
-                        } catch (Resources.NotFoundException ignored) {}
-                    }
-                }
-            });
+       //                          view.setVisibility(View.GONE);
+       //                          ZHRecyclerStat = false;
+       //                      }
+       //                  } catch (Resources.NotFoundException ignored) {}
+       //              }
+       //          }
+       //      });
         }
     }
 }

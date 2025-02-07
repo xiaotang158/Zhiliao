3a74df1a876aa39ff38a6aff2b479f8421f1635d
xiaotang158 <66454911+xiaotang158@users.noreply.github.com>
2025-02-08 04:31:50 +0800

    Update VIPBanner.java

--- app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
+++ app/src/main/java/com/shatyuka/zhiliao/hooks/VIPBanner.java
@@ -1,7 +1,6 @@
 package com.shatyuka.zhiliao.hooks;
 
 import android.content.res.Resources;
-import java.lang.reflect.Method;
 import android.view.View;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
@@ -9,9 +8,8 @@ import com.shatyuka.zhiliao.Helper;
 
 public class VIPBanner implements IHook {
      static Class<?> MoreHybridView;
-    // static Class<?> ZHRecyclerView;
-     static Method MonAttachedToWindow;
-    // static Method ZonAttachedToWindow;
+     static Class<?> ZHRecyclerView;
+
 
      boolean MoreHybridStat = true;
      boolean ZHRecyclerStat = true;
@@ -25,9 +23,7 @@ public class VIPBanner implements IHook {
     @Override
     public void init(ClassLoader classLoader) throws Throwable {
         MoreHybridView = classLoader.loadClass("com.zhihu.android.app.ui.fragment.more.more.widget.MoreHybridView");
-       // ZHRecyclerView = classLoader.loadClass("com.zhihu.android.base.widget.ZHRecyclerView");
-  
-
+        ZHRecyclerView = classLoader.loadClass("com.zhihu.android.base.widget.ZHRecyclerView");
       
     }
 
@@ -48,31 +44,27 @@ public class VIPBanner implements IHook {
                 }
             });
 
-       // XposedBridge.hookAllMethods(ZHRecyclerView, "onViewAdded", new XC_MethodHook() {
+       XposedBridge.hookAllMethods(ZHRecyclerView, "onScrollChanged", new XC_MethodHook() {
 
-       //          @Override
-       //          protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-       //                                                XposedBridge.log("[Zhiliao3] " + param);
-
-       //              super.afterHookedMethod(param);
-       //              if (ZHRecyclerStat && param.thisObject.getClass().getName().equals(ZHRecyclerView.getName())) {
-       //                                                                             XposedBridge.log("[Zhiliao4] " + param.thisObject);
-
-       //                  View view = (View) param.thisObject;
-       //                  try {
-       //                      String name = view.getContext().getResources().getResourceEntryName(view.getId());
-       //                      if ("function_panel".equals(name) ||
-       //                          "new_function_panel".equals(name) ||
-       //                          "common_items".equals(name) ||
-       //                          "new_common_items".equals(name)) {
+                @Override
+                protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                    super.afterHookedMethod(param);
+                    if (ZHRecyclerStat) {
+                        View view = (View) param.thisObject;
+                        try {
+                            String name = view.getContext().getResources().getResourceEntryName(view.getId());
+                            if ("function_panel".equals(name) ||
+                                "new_function_panel".equals(name) ||
+                                "common_items".equals(name) ||
+                                "new_common_items".equals(name)) {
                                 
-       //                          view.setVisibility(View.GONE);
-       //                          ZHRecyclerStat = false;
-       //                      }
-       //                  } catch (Resources.NotFoundException ignored) {}
-       //              }
-       //          }
-       //      });
+                                view.setVisibility(View.GONE);
+                                ZHRecyclerStat = false;
+                            }
+                        } catch (Resources.NotFoundException ignored) {}
+                    }
+                }
+            });
         }
     }
 }
